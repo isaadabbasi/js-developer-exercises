e6eaae893c1b67f80c3016740714b4e15530c3ec
Saad Abbasi <itssaadabbasi@gmail.com>
2017-02-28 22:08:42 +0500

    dev-exercises complete

--- src/readFile.js
+++ src/readFile.js
@@ -1,3 +1,14 @@
-module.exports = function () {
+var fs = require('fs'),
+    q = require('q');
+
+var readFile = function (filepath) {
+    var proInitialized = q.defer();
+    fs.readFile( filepath, 'utf-8', function(err, content){
+        if(err) proInitialized.reject('no such file')
+
+        if(content) proInitialized.resolve(JSON.parse(content))
+    })
+    return proInitialized.promise;
 
 }
+module.exports = readFile
